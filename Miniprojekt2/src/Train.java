58fb34023e4a06fd73b1404c4ecc19a94e810ac3
DaZorro75 <44848344+DaZorro75@users.noreply.github.com>
2018-11-07 21:10:04 +0100

    Add files via upload

---
+++ Miniprojekt2/src/Train.java
@@ -0,0 +1,93 @@
+public class Train {
+
+	// Kann für die Ausgabe verwendet werden
+	private static final String LOCOMOTIVE = "<___|o|";
+
+	private Waggon head;
+
+	public int getSize() {
+		int size = 0;
+		Waggon train0 = head;
+		while (train0 != null) {
+			train0.getNext();
+			size += 1;
+		}
+		
+		return size;
+	}
+
+	public int getPassengerCount() {
+		int passengers = 0;
+		Waggon train0 = head;
+		while (train0 != null ) {
+			passengers += train0.getPassengers();
+		}
+		return passengers;
+	}
+
+	public int getCapacity() {
+		int capacity = 0;
+		Waggon train0 = head;
+		while (train0 != null) {
+			capacity += train0.getCapacity();
+		}
+		return capacity;
+	}
+
+	public void appendWaggon(Waggon waggon) {
+		Waggon zug = head;
+		if (head == null) 
+		{
+			head = waggon;
+			zug = head;
+		}
+		else {
+			if(head.getNext() == null) {
+		}
+			head.setNext(waggon);
+			head.getNext();
+	
+		System.out.println(zug.toString());
+			}
+	}
+	
+
+	public void boardPassengers(int numberOfPassengers) {
+	}
+
+	public Train uncoupleWaggons(int index) {
+		return new Train();
+	}
+
+	public void insertWaggon(Waggon waggon, int index) {
+	}
+
+	public void turnOver() {
+	}
+
+	public void removeWaggon(Waggon waggon) {
+	}
+
+	public Waggon getWaggonAt(int index) {
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		if (getSize() == 0) {
+			return LOCOMOTIVE;
+		}
+
+		StringBuilder str = new StringBuilder(LOCOMOTIVE);
+
+		Waggon pointer = head;
+		while (pointer != null) {
+			str.append(" <---> ");
+			str.append(pointer.getName());
+			pointer = pointer.getNext();
+		}
+
+		return str.toString();
+	}
+
+}
