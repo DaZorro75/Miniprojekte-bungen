c99ce521ef6ac71972a7a1883db3090ebacdcd1f
DaZorro75 <julians1mail@googlemail.com>
2018-11-20 20:58:25 +0100

    Added "InsertPerson()".

---
+++ LED/src/Test.java
@@ -0,0 +1,13 @@
+package src;
+
+import ledControl.BoardController;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BoardController controller = BoardController.getBoardController();
+		controller.setBackgroundColor(0, 83, 135);
+	}
+
+}
